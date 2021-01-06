5094663e5e4e4dd72c083a2c375d45b7b3747721
Katrine <KatsCoding@users.noreply.github.com>
2021-01-06 14:31:51 +0100

    changed pom.xml to be correct, as I forgot earlier

--- src/main/java/Game/Dice.java
+++ src/main/java/Game/Dice.java
@@ -1,4 +1,16 @@
 package Game;
 
 public class Dice {
+    private final int MAX = 6;
+    private int eyeValue;
+
+    public int roll(){
+        eyeValue = (int)(Math.random() * MAX) + 1;
+        return eyeValue;
+    }
+
+    public int getEyeValue() {
+        return eyeValue;
+    }
+
 }

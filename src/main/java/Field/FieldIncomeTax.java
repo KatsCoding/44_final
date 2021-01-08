4bce53545276d76c669517e6ad2ad3989f020cd3
schnack12 <71651912+schnack12@users.noreply.github.com>
2021-01-08 17:38:52 +0100

    Override the methods that will not be used, because the classes cannot be owned and so on

--- src/main/java/Field/FieldIncomeTax.java
+++ src/main/java/Field/FieldIncomeTax.java
@@ -3,6 +3,46 @@ package Field;
 import Game.Player;
 
 public class FieldIncomeTax extends Fields{
+    @Override
+    public void setOwner(Player owner) {
+
+    }
+
+    @Override
+    public boolean getOwned() {
+        return false;
+    }
+
+    @Override
+    public void setOwned(boolean b) {
+
+    }
+
+    @Override
+    public int getStreetPrice() {
+        return 0;
+    }
+
+    @Override
+    public char getType() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentRent() {
+        return 0;
+    }
+
+    @Override
+    public void setCurrentRent(int Houses) {
+
+    }
+
+    @Override
+    public void setRentPriceMultiplier(int b) {
+
+    }
+
     FieldIncomeTax (String name, int price){
 
     }

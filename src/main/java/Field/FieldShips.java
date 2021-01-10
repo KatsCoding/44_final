ad5cf49125c7a6c13b7b4304ada158bdb247a6df
schnack12 <71651912+schnack12@users.noreply.github.com>
2021-01-10 18:18:29 +0100

    DisplayPrice for ships are now a thing

--- src/main/java/Field/FieldShips.java
+++ src/main/java/Field/FieldShips.java
@@ -5,13 +5,15 @@ import Game.Player;
 public class FieldShips extends Fields {
     int price;
     String propertynavn;
+    String displayPrice;
     boolean owned;
     Player owner;
     char type;
 
-    public FieldShips( String propertyName,int price, boolean owned, Player owner, char type) {
+    public FieldShips( String propertyName,String displayPrice,int price, boolean owned, Player owner, char type) {
         this.price = price;
         this.propertynavn = propertyName;
+        this.displayPrice = displayPrice;
         this.owned = owned;
         this.owner = owner;
         this.type = type;

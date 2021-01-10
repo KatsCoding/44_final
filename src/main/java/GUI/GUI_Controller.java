e4fc3f80892e15e449027f1b603cb58c8f6e92ae
steveng85 <71653345+steveng85@users.noreply.github.com>
2021-01-10 18:50:52 +0100

    made a method for setborder color to see if it works.

--- src/main/java/GUI/GUI_Controller.java
+++ src/main/java/GUI/GUI_Controller.java
@@ -1,5 +1,6 @@
 package GUI;
 import Field.Gameboard;
+import gui_fields.GUI_Ownable;
 import gui_main.GUI;
 
 import gui_fields.GUI_Field;
@@ -12,7 +13,7 @@ public class GUI_Controller {
     public static void main(String[] args){
 
         GUI_Field[] fields = GUI_game.makeGUIFields();
-
+        ((GUI_Ownable)fields[0]).setBorder(Color.BLACK, Color.BLACK);
         GUI gui = new GUI(fields, Color.WHITE);
 
 

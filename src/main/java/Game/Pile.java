4d5416ae852bc7dad2b5ef500f0bf1b0ed0e8790
OlineMelindaAndersen <71652011+OlineMelindaAndersen@users.noreply.github.com>
2021-01-11 13:56:12 +0100

    added more cards to pile

--- src/main/java/Game/Pile.java
+++ src/main/java/Game/Pile.java
@@ -41,6 +41,7 @@ public class Pile {
         addCard( new SimpleMoveToSpecificField("Ryk frem til Vimmelskaftet", "Vimmelskaftet", true));
         addCard( new SimpleMoveToSpecificField("Ryk frem til Strandvejen", "Strandvejen", true));
         addCard( new SimpleMoveToSpecificField("Tag til rådhuspladsen", "Frederiksberg Allé", true));
+        addCard( new SimpleMoveToSpecificField("Tag med Mols-linien, flyt brikken frem", "Mols-linien", true));
         addCard( new SimpleReceiveCash("Du har vundet i klasselotteriet. Du modtager 500 kr", 500));
         addCard( new SimpleReceiveCash("Du har vundet i klasselotteriet. Du modtager 500 kr", 500));
         addCard( new SimpleReceiveCash("Du modtager dit aktieudbytte. Du modtager 1000 kr", 1000));
@@ -53,6 +54,15 @@ public class Pile {
         addCard( new SimpleReceiveCash("Deres præmieobligation er udtrykket. Du modtager 1000 kr", 1000));
         addCard( new SimpleReceiveCash("Du har solgt nogle gamle møbler på auktion. Du modtager 1000", 1000));
         addCard( new SimpleReceiveCash("Værdien af egen avl fra nyttehaven udgør 200 som du får af banken", 200));
+        addCard( new SimplePayCash("Du har kørt frem for 'fuldt stop', betal 1000 kr i bøde", -1000 ));
+        addCard( new SimplePayCash("Betal 300 kr for vognvask og smøring", -300 ));
+        addCard( new SimplePayCash("Betal 3000 kr for reperation af din vogn", -3000 ));
+        addCard( new SimplePayCash("Betal 3000 kr for reperation af din vogn", -3000 ));
+        addCard( new SimplePayCash("Du har købt 4 nye dæk til din vogn. Du skal betale 1000 kr", -1000 ));
+        addCard( new SimplePayCash("Du har fået en parkeringsbøde, betal 200 kr i bøde", -200 ));
+        addCard( new SimplePayCash("Betal 1000 kr for din bilforsikring", -1000 ));
+        addCard( new SimplePayCash("Du har været udenlands og købt for mange smøger. Betal 200 kr i told", -200 ));
+        addCard( new SimplePayCash("Du skal betale 2000 kr for din tandlægeregning", -2000 ));
 
 
     }
@@ -60,4 +70,4 @@ public class Pile {
 
 }
 
-// KORT DER MANGLES: 1,2,20,
+// KORT DER MANGLES: 1,2,20,28,32

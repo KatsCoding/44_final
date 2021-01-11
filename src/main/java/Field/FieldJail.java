38118ddf3ec668f0ce8637c9e610426cc44c8f3a
schnack12 <71651912+schnack12@users.noreply.github.com>
2021-01-11 11:59:55 +0100

    fixed my own mistakes

---
+++ src/main/java/Field/FieldJail.java
@@ -0,0 +1,80 @@
+package Field;
+
+import Game.Player;
+
+public class FieldJail extends Fields{
+    String propertyName;
+
+    public FieldJail(String name) { propertyName = name; }
+
+    @Override
+    public Player getOwner() {
+        return null;
+    }
+
+    @Override
+    public void setOwner(Player owner) {
+
+    }
+
+    @Override
+    public boolean getOwned() {
+        return false;
+    }
+
+    @Override
+    public void setOwned(boolean b) {
+
+    }
+
+    @Override
+    public int getStreetPrice() {
+        return 0;
+    }
+
+    @Override
+    public char getType() {
+        return 0;
+    }
+
+    @Override
+    public int getCurrentRent() {
+        return 0;
+    }
+
+    @Override
+    public void setCurrentRent(int Houses) {
+
+    }
+
+    @Override
+    public void setRentPriceMultiplier(int b) {
+
+    }
+
+    @Override
+    public void setRentPrice(int currentRent) {
+
+    }
+
+    @Override
+    public String getPropertyName() {
+        return null;
+    }
+
+    @Override
+    public String FieldStart() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayPrice() {
+        return null;
+    }
+
+    @Override
+    public void setDisplayPrice(String displayPrice) {
+
+    }
+}
+

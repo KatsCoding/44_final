fd2291bab2c7239cb8e234ec97e59e4c73f3a778
steveng85 <71653345+steveng85@users.noreply.github.com>
2021-01-12 16:58:48 +0100

    made changes in gui_game and Paytaxes.

--- src/main/java/GUI/GUI_game.java
+++ src/main/java/GUI/GUI_game.java
@@ -18,7 +18,7 @@ public class GUI_game {
         gui_fields[1] = new GUI_Street(gameboard.getArray()[1].getPropertyName(), gameboard.getArray()[1].getDisplayPrice(), "", gameboard.getArray()[1].getDisplayPrice(), new Color(44,14, 235), Color.WHITE);
         gui_fields[2] = new GUI_Chance( "?","Prøv lykken" ,"" , Color.BLACK, Color.WHITE );
         gui_fields[3] = new GUI_Street(gameboard.getArray()[3].getPropertyName(),gameboard.getArray()[3].getDisplayPrice() ,"" , gameboard.getArray()[3].getDisplayPrice(), new Color(44,14, 235), Color.WHITE);
-        gui_fields[4] = new GUI_Tax("","" ,"" , Color.WHITE,  Color.WHITE);
+        gui_fields[4] = new GUI_Tax("Betal-\n"+"indkomstskat:\n"+"10% eller\n"+"4000 kr."," " ,"" , Color.WHITE,  Color.BLACK);
         gui_fields[5] = new GUI_Shipping("default",gameboard.getArray()[5].getPropertyName() ,gameboard.getArray()[5].getDisplayPrice(), "", "" , new Color(44,14, 235), Color.WHITE);
         gui_fields[6] = new GUI_Street(gameboard.getArray()[6].getPropertyName(),gameboard.getArray()[6].getDisplayPrice() ,"" , gameboard.getArray()[6].getDisplayPrice(), new Color(238, 45, 18), Color.WHITE);
         gui_fields[7] = new GUI_Chance( "?","Prøv lykken" ,"" ,Color.BLACK ,Color.WHITE );
@@ -26,7 +26,7 @@ public class GUI_game {
         gui_fields[9] = new GUI_Street(gameboard.getArray()[9].getPropertyName(), gameboard.getArray()[9].getDisplayPrice() ,"" ,gameboard.getArray()[9].getDisplayPrice(),  new Color(238, 45, 18), Color.WHITE);
         gui_fields[10] = new GUI_Jail("default","" ,"" , "",Color.WHITE  ,Color.WHITE);
         gui_fields[11] = new GUI_Street(gameboard.getArray()[11].getPropertyName(),gameboard.getArray()[11].getDisplayPrice() ,"" ,gameboard.getArray()[11].getDisplayPrice(), new Color(168, 114, 8), Color.WHITE);
-        gui_fields[12] = new GUI_Brewery("default","" ,"" , "", "" ,Color.WHITE ,Color.WHITE);
+        gui_fields[12] = new GUI_Brewery("default","" ,"3000 kr." , "", "" ,new Color(23, 255, 0)  ,Color.BLACK);
         gui_fields[13] = new GUI_Street(gameboard.getArray()[13].getPropertyName(),gameboard.getArray()[13].getDisplayPrice() ,"" ,gameboard.getArray()[13].getDisplayPrice(),  new Color(168, 114, 8), Color.WHITE);
         gui_fields[14] = new GUI_Street(gameboard.getArray()[14].getPropertyName(),gameboard.getArray()[14].getDisplayPrice() ,"" ,gameboard.getArray()[14].getDisplayPrice(),  new Color(168, 114, 8), Color.WHITE);
         gui_fields[15] = new GUI_Shipping("default",gameboard.getArray()[15].getPropertyName() ,gameboard.getArray()[15].getDisplayPrice() , "", "" , new Color(255, 31 , 31),Color.WHITE);
@@ -42,7 +42,7 @@ public class GUI_game {
         gui_fields[25] = new GUI_Shipping("default",gameboard.getArray()[25].getPropertyName() ,gameboard.getArray()[25].getDisplayPrice() , "", "" , new Color(44,14, 235), Color.WHITE);
         gui_fields[26] = new GUI_Street(gameboard.getArray()[26].getPropertyName(),gameboard.getArray()[26].getDisplayPrice() ,"" , gameboard.getArray()[26].getDisplayPrice(),  new Color(255, 255, 255), Color.BLACK);
         gui_fields[27] = new GUI_Street(gameboard.getArray()[27].getPropertyName(),gameboard.getArray()[27].getDisplayPrice() ,"" , gameboard.getArray()[27].getDisplayPrice(),  new Color(255, 255, 255), Color.BLACK);
-        gui_fields[28] = new GUI_Brewery("default","" ,"" , "", "" , Color.WHITE,Color.WHITE);
+        gui_fields[28] = new GUI_Brewery("default","" ,"3000 kr." , "", "" , new Color(23, 255, 0),Color.BLACK);
         gui_fields[29] = new GUI_Street(gameboard.getArray()[29].getPropertyName(),gameboard.getArray()[29].getDisplayPrice() ,"" , gameboard.getArray()[29].getDisplayPrice(),  new Color(255, 255, 255), Color.BLACK);
         gui_fields[30] = new GUI_Jail("default","" ,"" ,"" ,Color.WHITE  , Color.WHITE);
         gui_fields[31] = new GUI_Street(gameboard.getArray()[31].getPropertyName(),gameboard.getArray()[31].getDisplayPrice() ,"" , gameboard.getArray()[31].getDisplayPrice(),  new Color(255, 193, 0), Color.WHITE);
@@ -52,7 +52,7 @@ public class GUI_game {
         gui_fields[35] = new GUI_Shipping("default",gameboard.getArray()[35].getPropertyName() ,gameboard.getArray()[35].getDisplayPrice() , "", "" ,new Color(44,14, 235) ,Color.WHITE);
         gui_fields[36] = new GUI_Chance( "?","Prøv lykken" ,"" , Color.BLACK, Color.WHITE);
         gui_fields[37] = new GUI_Street(gameboard.getArray()[37].getPropertyName(),gameboard.getArray()[37].getDisplayPrice() ,"" , gameboard.getArray()[37].getDisplayPrice(), new Color(104, 3, 87), Color.WHITE);
-        gui_fields[38] = new GUI_Tax("","" ,"" , Color.WHITE, Color.WHITE );
+        gui_fields[38] = new GUI_Tax("Ekstraordinær-\n"+"statsskat: \n"+"betal 2000 kr.","" ,"" , Color.WHITE, Color.BLACK );
         gui_fields[39] = new GUI_Street(gameboard.getArray()[39].getPropertyName(),gameboard.getArray()[39].getDisplayPrice() ,"" , gameboard.getArray()[39].getDisplayPrice(), new Color(104, 3, 87), Color.WHITE);
 
         return gui_fields;
--- src/main/java/Game/PayTaxes.java
+++ src/main/java/Game/PayTaxes.java
@@ -1,15 +1,24 @@
 package Game;
 
+
+import Field.Gameboard;
+
 public class PayTaxes {
 
     private double moneyLeft;
     private final int price = 4000;
 
+    /*Gameboard gameboard = new Gameboard();
+    Player player = new Player(currentPlayer, id);
 
-   /* public double incomeTax(int i){
-        value = cash+houseValue+hotelValue+mortgage;
+    public double incomeTax(boolean sandtEllerFalsk){
+   int houseValue = gameboard.getArray()[i].getHouse();
+   int hotelValue = gameboard.getArray()[i].getHotel();
+   int cash = player.getCash();
 
-    if (i == 0 ){
+        int value = cash+houseValue+hotelValue+mortgage;
+
+    if (boolean sandtEllerFalsk == true){
         moneyLeft =value*0.1;
     }else{
         moneyLeft = value-price;

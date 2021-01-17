e7945fa0f1b67cdfc26759387e35391ac681a9ec
steveng85 <71653345+steveng85@users.noreply.github.com>
2021-01-18 00:45:51 +0100

    made method for extra ordinary tax

--- src/main/java/Game/PayTaxes.java
+++ src/main/java/Game/PayTaxes.java
@@ -1,13 +1,11 @@
 package Game;
-
-
-import Field.FieldStreet;
 import Field.Gameboard;
 
 public class PayTaxes {
 
     private int moneyLeft;
     private final int price = 4000;
+    private final int tax = 2000;
     private int mortgage;
     private int balance;
     private int value;
@@ -25,6 +23,7 @@ public class PayTaxes {
             if (gameboard.getArray()[j].getOwner() == currentPlayer) {// check if the current player ownes the field
                 int houseValue =+ gameboard.getArray()[j].getHouses() * gameboard.getArray()[j].getHousePrice();//get house and house price on field and multiply it
                 int fieldValue =+ gameboard.getArray()[j].getStreetPrice();//get field price
+
                 if (gameboard.getArray()[j].useMortgage()) {//check if mortgage is used on a field if there is, it get the mortgage price.
                     mortgage =+ gameboard.getArray()[j].getMortgage();
                     return mortgage;
@@ -35,12 +34,20 @@ public class PayTaxes {
         }
 
         balance = cash + mortgage + value;
-        if (bottonInput == "10%"){
-        moneyLeft = (int) (balance*0.1);
+        if (bottonInput.equals("10%")){
+        moneyLeft = cash - (int) (balance*0.1);
     }else{
         moneyLeft = cash-price;
         }
     return moneyLeft;
+    }
+
+    public int extraOrdinaryTax(){
+        int cash = currentPlayer.getCash();// get the balance of the current player
+
+        moneyLeft = cash - tax;
+
+        return moneyLeft;
     }
 
 }

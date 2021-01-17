a03398e6fef3d7f6476f7fb616d9f952ee995c9d
Katrine <KatsCoding@users.noreply.github.com>
2021-01-17 03:26:58 +0100

    changed StreetFieldTest so it now tests correctly (and it works, yay)

--- src/test/java/Game/StreetFieldTest.java
+++ src/test/java/Game/StreetFieldTest.java
@@ -5,6 +5,7 @@ import Field.FieldShips;
 import Field.FieldStreet;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,8 +26,11 @@ public class StreetFieldTest {
     void testBuildMaxFourHouses() {
         Player player = new Player("Simon");
         FieldStreet tester = new FieldStreet("Allégade", "2400", 'b', false, 2400, new int[]{150, 800, 2000, 6000, 9000, 12000}, 1000, null, 3);
+
+
         assertEquals(0,tester.getHouses());
         tester.setOwner(player);
+        tester.setCanBuild(true);
         assertEquals(true, tester.canBuildHouse());
 
         assertEquals(true,tester.buildHouse());
@@ -44,17 +48,17 @@ public class StreetFieldTest {
         assertEquals(4,tester.getHouses());
         assertEquals(tester.getCurrentRent(),9000);
 
-        assertEquals(true,tester.canBuildHouse());
-        assertEquals(true,tester.buildHouse());
+        assertEquals(false,tester.canBuildHouse());
+        assertEquals(false,tester.buildHouse());
         assertEquals(4,tester.getHouses());
     }
 
     @Test
     void testCannotBuildWithoutOwner() {
-
         FieldStreet tester = new FieldStreet("Allégade", "2400", 'b', false, 2400, new int[]{150, 800, 2000, 6000, 9000, 12000}, 1000, null, 3);
         assertEquals(false, tester.canBuildHouse());
     }
+
     @Test
     void testCanLinkFields(){
         FieldStreet testStreetBlue1 = new FieldStreet("Allégade", "2400", 'b', false, 2400, new int[]{150, 800, 2000, 6000, 9000, 12000}, 1000, null, 3);
@@ -84,14 +88,12 @@ public class StreetFieldTest {
         FieldStreet testStreetBlue3 = new FieldStreet("Allégade", "2400", 'b', false, 2400, new int[]{150, 800, 2000, 6000, 9000, 12000}, 1000, null, 3);
 
         FieldStreet.linkFields(new FieldStreet[]{testStreetBlue1, testStreetBlue2, testStreetBlue3});
-
     }
 
     @Test
     void testBuyShipAndCola() {
         FieldShips tester = new FieldShips("mols","4000",4000,false,null,'b');
         FieldBrewery tester2 = new FieldBrewery("cola",4000,false,null,'a');
-
 
         assertEquals("mols", tester.getPropertyName());
         assertEquals(4000,tester.getPrice());
